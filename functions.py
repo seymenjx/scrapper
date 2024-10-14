b589b74d2355e44cc1740becab83ff5b83f811ad
seymen <seymenozcelk@gmail.com>
2024-10-14 11:10:32 +0300

    Updated Chrome and ChromeDriver setup for Heroku

--- functions.py
+++ functions.py
@@ -571,5 +571,8 @@ def get_chrome_driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+    service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+    
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
